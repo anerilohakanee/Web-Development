03b59337ee77bb7283c596e707981805bf3c50ea
anerilohakanee <lohakaneaneri6@gmail.com>
2024-12-26 18:20:01 +0530

    First Commit

---
+++ javaScript/intro2.js
@@ -0,0 +1,10 @@
+console.log("Hello World");
+document.write("______________________________<b>from Aneri</b> <br><br> what's going on?");
+window.alert("Do you want to continue...? ");
+//document.write(10+20);
+
+x= 10;
+y = "<br>JavaScript";
+
+document.write("<br>" + x);
+document.write(y);
